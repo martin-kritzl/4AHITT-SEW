f125de4df37253ced789072b6103bc7d11063840
tgm-mkritzl <mkritzl@student.tgm.ac.at>
2015-01-15 18:28:59 +0100

    ant test

---
+++ SEW-Build_Automation2/build/src/at/mkritzl/ant/data/Hello.java
@@ -0,0 +1,15 @@
+package at.mkritzl.ant.data;
+
+/**
+ * Created by Martin Kritzl on 08.01.2015.
+ */
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("Hallo Welt");
+
+    }
+
+    public String getName() {
+        return "Martin";
+    }
+}
