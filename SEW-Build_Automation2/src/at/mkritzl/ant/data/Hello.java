5beff6e990140dd1a867643d18aa78715dcb7937
tgm-mkritzl <mkritzl@student.tgm.ac.at>
2015-01-15 18:20:42 +0100

    Added SEW-Build_Automation2

---
+++ SEW-Build_Automation2/src/at/mkritzl/ant/data/Hello.java
@@ -0,0 +1,15 @@
+package at.mkritzl.ant.data;
+
+/**
+ * Created by Martin Kritzl on 08.01.2015.
+ */
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("%helloworldtext%");
+
+    }
+
+    public String getName() {
+        return "Martin";
+    }
+}
